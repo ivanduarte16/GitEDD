413fe74896077cfb909d8f5a8ff63f3b7d16af56
ivanduarte16 <ivanduortiz2000@gmail.com>
2021-01-12 13:12:33 +0100

    afegir arxius

---
+++ Unitat 4/Calcula/calc.h
@@ -0,0 +1,11 @@
+#ifndef MYCALC
+#define MYCALC
+
+
+int suma(int op1, int op2);
+int resta(int op1, int op2);
+int multiplica(int op1, int op2);
+int divideix(int op1, int op2);
+int mayor(int op1, int op2);
+
+#endif
